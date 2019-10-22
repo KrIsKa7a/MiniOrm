7561323d30960b92e4e098a770ef4fb1ee9fdb6a
KrIsKa7a <krisi>
2019-10-22 12:30:01 +0300

    Add functionality for changes tracing and creating generic DbSets.

---
+++ MiniORM/ChangeTracker.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MiniORM
+{
+	internal class ChangeTracker<T>
+        where T : class, new()
+    {
+        private readonly List<T> allEntities;
+
+        private readonly List<T> added;
+
+        private readonly List<T> removed;
+
+        public ChangeTracker(IEnumerable<T> entities)
+        {
+            this.added = new List<T>();
+            this.removed = new List<T>();
+
+            this.allEntities = CloneEntities(entities);
+        }
+
+        public IReadOnlyCollection<T> AllEntities => 
+            this.allEntities.AsReadOnly();
+
+        public IReadOnlyCollection<T> Added =>
+            this.added.AsReadOnly();
+
+        public IReadOnlyCollection<T> Removed =>
+            this.removed.AsReadOnly();
+
+        public void Add(T item)
+        {
+            this.added.Add(item);
+        }
+
+        public void Remove(T item)
+        {
+            this.removed.Add(item);
+        }
+
+        public IEnumerable<T> GetModifiedEntities(DbSet<T> dbSet)
+        {
+            List<T> modifiedEntities = new List<T>();
+
+            PropertyInfo[] primaryKeys = typeof(T)
+                .GetProperties()
+                .Where(pi => pi.HasAttribute<KeyAttribute>())
+                .ToArray();
+
+            foreach (T proxyEntity in this.AllEntities)
+            {
+                object[] primaryKeyValues = GetPrimaryKeyValues(primaryKeys, proxyEntity).ToArray();
+
+                T entity = dbSet
+                    .Entities
+                    .Single(e => GetPrimaryKeyValues(primaryKeys, e)
+                                .SequenceEqual(primaryKeyValues));
+
+                //TODO: May be swapped
+                //Here first one is proxy, second is original
+                //In method args first is original, second is proxy
+                bool isModified = IsModified(proxyEntity, entity);
+
+                if (isModified)
+                {
+                    modifiedEntities.Add(entity);
+                }
+            }
+
+            return modifiedEntities;
+        }
+
+        private static IEnumerable<object> GetPrimaryKeyValues(IEnumerable<PropertyInfo> primaryKeys, T entity)
+        {
+            return primaryKeys
+                .Select(pk => pk.GetValue(entity));
+        }
+
+        private static bool IsModified(T entity, T proxyEntity)
+        {
+            IEnumerable<PropertyInfo> monitoredProperties = typeof(T)
+                .GetProperties()
+                .Where(pi => DbContext.AllowedSqlTypes.Contains(pi.PropertyType));
+
+            PropertyInfo[] modifiedProperties = monitoredProperties
+                .Where(pi =>
+                !Equals(pi.GetValue(entity), pi.GetValue(proxyEntity)))
+                .ToArray();
+
+            bool isModified = modifiedProperties.Any();
+
+            return isModified;
+        }  
+
+        private static List<T> CloneEntities(IEnumerable<T> entities)
+        {
+            List<T> clonedEntities = new List<T>();
+
+            PropertyInfo[] propertiesToClone = typeof(T)
+                .GetProperties()
+                .Where(pi => DbContext.AllowedSqlTypes.Contains(pi.PropertyType))
+                .ToArray();
+
+            foreach (T entity in entities)
+            {
+                T clonedEntity = Activator.CreateInstance<T>();
+
+                foreach (PropertyInfo property in propertiesToClone)
+                {
+                    object value = property.GetValue(entity);
+                    property.SetValue(clonedEntity, value);
+                }
+
+                clonedEntities.Add(clonedEntity);
+            }
+
+            return clonedEntities;
+        }
+    }
+}
---
+++ MiniORM/DbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace MiniORM
+{
+	public abstract class DbContext
+    {
+        private readonly DatabaseConnection connection;
+
+        private readonly Dictionary<Type, PropertyInfo> dbSetProperties;
+
+        internal static readonly Type[] AllowedSqlTypes =
+        {
+            typeof(string),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(decimal),
+            typeof(bool),
+            typeof(DateTime)
+        };
+
+
+    }
+}
