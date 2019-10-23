cbc853cbd42db97a12c0e1d382c33c1dc9b0d6fc
KrIsKa7a <kristiqn.ivanov@abv.bg>
2019-10-23 15:10:59 +0300

    Add DbContext functionality and test app for the framework.

---
+++ MiniORM.App/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+
+namespace MiniORM.App
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=DESKTOP-F0IMF0D\\SQLEXPRESS;" +
+                "Database=MiniORM;" +
+                "Integrated Security=true";
+
+            SoftUniDbContext context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+                FirstName = "Gosho",
+                LastName = "Inserted",
+                DepartmentId = context.Departments.First().Id,
+                IsEmployed = true
+            });
+
+            Employee employee = context.Employees.Last();
+            employee.FirstName = "Modified";
+
+            context.SaveChanges();
+        }
+    }
+}
